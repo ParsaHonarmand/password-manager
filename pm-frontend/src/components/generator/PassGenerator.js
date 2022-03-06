8a9a248d95e4cef2bcfda7e1fe39af5afa2b3772
Timothy Macphail <timothy.macphail@gmail.com>
2022-03-05 18:00:09 -0700

    feat: pass generator uses MUI components

--- pm-frontend/src/components/generator/PassGenerator.js
+++ pm-frontend/src/components/generator/PassGenerator.js
@@ -1,27 +1,16 @@
 //import * as React from "react";
 import React, { useState } from "react";
 import {
-  AppBar,
   Box,
-  Toolbar,
   Typography,
-  Button,
-  IconButton,
-  ListItem,
-  ListItemText,
-  Link,
   Slider,
-  Stack,
   FormControl,
   RadioGroup,
   FormControlLabel,
   Radio,
-  FormLabel,
 } from "@mui/material";
 
 export default function PassGenerator() {
-  const [selectedValue, setSelectedValue] = React.useState("a");
-
   const [passType, setPassType] = useState("word");
 
   const [length, setValue] = React.useState(12);
@@ -30,45 +19,49 @@ export default function PassGenerator() {
   };
   return (
     <Box>
-      <Radio
-        checked={selectedValue === "a"}
-        onChange={() => setSelectedValue("a")}
-        value="a"
-        name="radio-buttons"
-        inputProps={{ "aria-label": "A" }}
-      />
-      <Radio
-        checked={selectedValue === "b"}
-        onChange={() => setSelectedValue("b")}
-        value="b"
-        name="radio-buttons"
-        inputProps={{ "aria-label": "B" }}
-      />
+      <FormControl sx={{ mt: 5 }}>
+        <RadioGroup row name="row-radio-buttons-group">
+          <FormControlLabel
+            value="word"
+            checked={passType === "word"}
+            control={<Radio onChange={() => setPassType("word")} />}
+            label="word"
+          />
+          <FormControlLabel
+            value="phrase"
+            checked={passType === "phrase"}
+            control={<Radio onChange={() => setPassType("phrase")} />}
+            label="phrase"
+          />
+        </RadioGroup>
+      </FormControl>
       <Typography
         variant="h4"
         component="div"
         gutterBottom
         style={{ color: "#404040" }}
       >
-        Generating Pass{passType}
+        Generating pass{passType}
       </Typography>
 
-      <Stack alignItems="center">
-        <Slider
-          aria-label="Password Length"
-          sx={{ width: 1 / 2 }}
-          valueLabelDisplay="on"
-          value={length}
-          onChange={handleLengthChange}
-          min={8}
-          max={20}
-        />
-      </Stack>
+      <Slider
+        aria-label="Password Length"
+        sx={{ width: 1 / 2 }}
+        valueLabelDisplay="on"
+        value={length}
+        onChange={handleLengthChange}
+        min={8}
+        max={20}
+      />
 
-      <p>Password Length: {length}</p>
-
-      <h1>Generated Password:</h1>
-      <p>password123</p>
+      <Typography
+        variant="h6"
+        component="div"
+        gutterBottom
+        style={{ color: "#404040" }}
+      >
+        Password length: {length}
+      </Typography>
     </Box>
   );
 }
