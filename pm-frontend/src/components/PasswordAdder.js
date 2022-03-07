1c7515c6df17279b46fde2bd4fd865e33476ac92
Timothy Macphail <timothy.macphail@gmail.com>
2022-03-07 08:51:33 -0700

    feat: add loading circle when saving passwords

--- pm-frontend/src/components/PasswordAdder.js
+++ pm-frontend/src/components/PasswordAdder.js
@@ -1,27 +1,28 @@
 import * as React from "react";
-// import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-// import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-// import FormControlLabel from "@mui/material/FormControlLabel";
-// import Checkbox from "@mui/material/Checkbox";
-// import Link from "@mui/material/Link";
-// import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-// import Container from "@mui/material/Container";
-// import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+  Button,
+  TextField,
+  Typography,
+  Box,
+  CircularProgress,
+} from "@mui/material";
+import { useState } from "react";
 
 export default function PasswordAdder() {
+  const [submitted, setSubmitted] = useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    setSubmitted(!submitted);
     const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+    setTimeout(
+      console.log({
+        email: data.get("website"),
+        password: data.get("password"),
+      }),
+      3000
+    );
   };
 
   return (
@@ -66,6 +67,14 @@ export default function PasswordAdder() {
           Save login
         </Button>
       </Box>
+      {submitted ? (
+        <>
+          <Typography>encrypting...</Typography>
+          <CircularProgress />
+        </>
+      ) : (
+        <></>
+      )}
     </Box>
   );
 }
