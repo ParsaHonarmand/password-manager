6db53167b51dffc9a008d0f46242b85fc8c060a3
parsahonarmand <parsa.honarmand@gmail.com>
2022-03-08 14:13:10 -0700

    backend setup with mongodb and listener port

---
+++ pm-backend/index.js
@@ -0,0 +1,39 @@
+require("dotenv").config()
+const express = require("express")
+const { MongoClient, ServerApiVersion } = require('mongodb')
+
+const app = express()
+app.use(express.json())
+
+
+const uri = `mongodb+srv://pw-manager:${process.env.MONGO_PW}@password-manager-cluste.kslqf.mongodb.net/password-manager-cluster?retryWrites=true&w=majority`
+const client = new MongoClient(
+    uri, 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        serverApi: ServerApiVersion.v1 
+    }
+);
+
+let db, users
+
+client.connect((err, client) => {
+    if (err) {
+        console.error("Error connecting to the DB: " + err)
+        return
+    }
+    console.log("Connected to DB")
+    db = client.db("password-manager-db")
+    users = db.collection("users")
+});
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from Express!" })
+});
+
+app.listen(3001, () => 
+    console.log("Server listening on 3001")
+)
+
