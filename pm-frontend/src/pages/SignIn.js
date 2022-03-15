fcd94918f31070ad7570bb03fccbb79b3da2879a
Timothy Macphail <timothy.macphail@gmail.com>
2022-03-15 09:13:35 -0600

    fix: remove redundant import of axios

--- pm-frontend/src/pages/SignIn.js
+++ pm-frontend/src/pages/SignIn.js
@@ -13,7 +13,6 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { useState } from "react";
-import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3001/",
