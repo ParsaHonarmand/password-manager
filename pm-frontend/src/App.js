772ee1530ff50dde0d0dc503a227c0dc6ffaa79f
Timothy Macphail <timothy.macphail@gmail.com>
2022-03-15 19:51:47 -0600

    feat: add signup page with routing

--- pm-frontend/src/App.js
+++ pm-frontend/src/App.js
@@ -2,11 +2,7 @@ import "./App.css";
 import PasswordAdder from "./components/PasswordAdder";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">Landing Page</div>;
 }
 
 export default App;
--- pm-frontend/src/index.js
+++ pm-frontend/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import Vault from "./Vault";
 import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import TopBar from "./components/TopBar";
 import Generator from "./Generator";
 import reportWebVitals from "./reportWebVitals";
@@ -16,6 +17,7 @@ ReactDOM.render(
       <Route path="/" element={<App />} />
       <Route path="/vault" element={<Vault />} />
       <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/generator" element={<Generator />} />
     </Routes>
   </Router>,
