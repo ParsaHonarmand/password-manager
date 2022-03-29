402fd4ffef697ac3b2f3ce7e8042a091cdf63134
parsahonarmand <parsa.honarmand@gmail.com>
2022-03-28 20:30:52 -0600

    password generator

--- pm-backend/queries/passwordGenerator.js
+++ pm-backend/queries/passwordGenerator.js
@@ -1,10 +1,24 @@
 require('dotenv').config()
-const { mongoClient } = require('../mongoClient')
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const { generate } = require('generate-passphrase')
+const generator = require('generate-password');
 
 const createPassword = (request, response) => {
     // here we create a strong password and return it to the frontend 
+    const length = request.body.length
+    const type = request.body.type
+    let generatedPassword
+    if (type === "word") {
+        generatedPassword = generator.generate({
+            length: length,
+            numbers: true,
+            symbols: true,
+            uppercase: true,
+        });
+        console.log(generatedPassword)
+    } else if (type === "phrase") {
+        generatedPassword = generate({length: length, separator: " ", titlecase: true}) 
+    }
+    response.status(200).send(generatedPassword)
 }
 
 module.exports = {
--- pm-frontend/src/components/generator/PassGenerator.js
+++ pm-frontend/src/components/generator/PassGenerator.js
@@ -1,5 +1,6 @@
 //import * as React from "react";
 import React, { useState } from "react";
+import axios from "axios";
 import {
   Box,
   Typography,
@@ -7,16 +8,30 @@ import {
   FormControl,
   RadioGroup,
   FormControlLabel,
-  Radio,
+  Radio, 
+  Button,
 } from "@mui/material";
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 export default function PassGenerator() {
   const [passType, setPassType] = useState("word");
 
   const [length, setValue] = React.useState(12);
+  const [password, setPassword] = React.useState("");
+
   const handleLengthChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const generatePassword = async (event) => {
+    try {
+      const res = await axios.post("http://localhost:3001/generatePassword", {length: length, type: passType})
+      setPassword(res.data)
+    } catch(error) {
+      console.log("Failed to generate password")
+      console.log(error)
+    }
+  }
   return (
     <Box>
       <FormControl sx={{ mt: 5 }}>
@@ -25,13 +40,13 @@ export default function PassGenerator() {
             value="word"
             checked={passType === "word"}
             control={<Radio onChange={() => setPassType("word")} />}
-            label="word"
+            label="Word"
           />
           <FormControlLabel
             value="phrase"
             checked={passType === "phrase"}
             control={<Radio onChange={() => setPassType("phrase")} />}
-            label="phrase"
+            label="Phrase"
           />
         </RadioGroup>
       </FormControl>
@@ -41,7 +56,7 @@ export default function PassGenerator() {
         gutterBottom
         style={{ color: "#404040" }}
       >
-        Generating pass{passType}
+        Generating Pass{passType}
       </Typography>
 
       <Slider
@@ -62,6 +77,55 @@ export default function PassGenerator() {
       >
         Password length: {length}
       </Typography>
+      <Button
+              type="submit"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={generatePassword}
+      >
+        Submit
+      </Button>
+      {password !== "" &&
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            p: 1,
+            m: 1,
+            bgcolor: "darkcyan",
+            borderRadius: 1,
+            width: "50%",
+            margin: "auto",
+            height: "15vh",
+            overflowY: "scroll"
+          }}
+        >
+          <Typography
+            variant="h4"
+            component="div"
+            gutterBottom
+            style={{ 
+              color: "#fff", 
+              marginRight: "10px", 
+              borderRight: "2px solid #fff", 
+              padding: "10px",
+            }}
+          >
+            Result
+            <Button color="inherit">
+              <ContentCopyIcon onClick={() => {navigator.clipboard.writeText(password)}} />
+            </Button>
+          </Typography>
+          <Typography
+            variant="h6"
+            component="div"
+            gutterBottom
+            style={{ color: "#fff", margin: "auto", padding: "5px" }}
+          >
+            <b>{password}</b>
+          </Typography>
+        </Box>
+      }
     </Box>
   );
 }
